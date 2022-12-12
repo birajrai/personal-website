b874279fc2dea8cac251f2af142259a3299935cf
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-12-12 19:08:40 +0545

    e

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,57 +1,41 @@
-let dateFns = require("date-fns")
+let dateFns = require("date-fns");
 
-let d = '31/10/2006'; // 10 Mar 1998
-let date = dateFns.parse(d, 'dd/MM/yyyy', new Date());
+let d = "31/10/2006"; // 10 Mar 1998
+let date = dateFns.parse(d, "dd/MM/yyyy", new Date());
 let age = dateFns.differenceInYears(new Date(), date);
 
 const About = () => {
   return (
-    <div className="section">
+    <div className="about">
       {/* About Me Start */}
       <div>
         <div>
-          <h1>About Me </h1>
+          <h1 className="about-title">About Me </h1>
         </div>
-        <div>
+        <div className="about-p">
           <p>
             Hello, my name is Biraj. Usually called Neppixel ( at the internet
             ). Born and live in Nepal.
           </p>
-          <p>I am a {age} year(s) old self-taught developer who is currently a student.</p>
+          <p>
+            I am a {age} year(s) old self-taught developer who is currently a
+            student.
+          </p>
           <p>
             I've worked with numberous techonlogies and projects, but I still
             think of myself as a humble beginner and I want to learn more and
             get better.
           </p>
-          <p>Having weird obsession with <a href="https://www.kritika.tk/" target="_blank">felix</a>.</p>
+          <p>
+            Having weird obsession with{" "}
+            <a className="link" href="https://www.kritika.tk/">
+              felix
+            </a>
+            .
+          </p>
         </div>
       </div>
       {/* About Me End */}
-
-      {/* Skills Start */}
-      <div>
-        {/* Skills Languages Start */}
-        <div id="languages" className="languages">
-          <h1>Languages</h1>
-          <p>Javascript</p>
-          <p>Java</p>
-          <p>TypeScript</p>
-          <p>YAML</p>
-        </div>
-        {/* Skills Languages End */}
-
-        {/* Skills Tools Start */}
-        <div id="tools" className="tools">
-          <h1>Tools</h1>
-          <p>Visual Studio Code</p>
-          <p>Intellij IDEA (aka JetBrain)</p>
-          <p>Git</p>
-          <p>Vim & Neovim</p>
-          <p>Figma ( no-idea )</p>
-        </div>
-        {/* Skills Tools End */}
-      </div>
-      {/* Skills End */}
     </div>
   );
 };
