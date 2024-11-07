428d3245ccd296728313d8f52d33b78e55764144
kathmandhu <70630924+kathmandhu@users.noreply.github.com>
2024-11-07 13:54:12 +0545

    Create TechCard.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,11 @@
 // src/pages/_app.js
+import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
